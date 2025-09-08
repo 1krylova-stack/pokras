55c00171e5d51dabb6314e44b38b62480ec5c19d
1krylova-stack <anastasia1991krylova@gmail.com>
2025-09-08 22:52:55 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -15,13 +15,13 @@
                 <p>От качества кузовного ремонта напрямую зависит внешний вид автомобиля, поэтому только опытные мастера смогут вернуть вашей машине ее первозданный вид.</p>
                 <p class="have_questions">Остались вопросы?</p>
                 <a class="button" href="#question_order">Задайте их мастеру по ремонту прямо сейчас</a>
-            </div><!--/.section_content-->
-        </div><!--/.contacts_info-->
+            </div>
+        </div>
         <div class="dtc contacts_map">
             <?php the_field('yandex_map', 'options'); ?>
         </div>
-    </div><!--/.dt-->
-</div><!--/#contacts-->
+    </div>
+</div>
 
 <footer id="footer">
     <div class="center_wrap">
@@ -30,16 +30,16 @@
                 <a href="/"><p>Покраска <span>Авто</span></p><span>Честный кузовной ремонт<br>с гарантией 1 год.</span></a>
                 <p class="copyright">Copyright © 2016-<?php echo date('Y'); ?> ООО «Покрас Авто» <br>Все права защищены</p>
                 <a class="conf" href="#conf">Политика конфиденциальности</a>
-            </div><!--/.footer_logo-->
+            </div>
             <div class="dtc vat">
                 <ul><?php wp_nav_menu(['menu' => 'Нижнее меню. Столбец 1', 'container' => '', 'items_wrap' => '%3$s']); ?></ul>
             </div>
             <div class="dtc vat">
                 <ul><?php wp_nav_menu(['menu' => 'Нижнее меню. Столбец 2', 'container' => '', 'items_wrap' => '%3$s']); ?></ul>
             </div>
-        </div><!--/.dt-->
-    </div><!--/.center_wrap-->
-</footer><!--/#footer-->
+        </div>
+    </div>
+</footer>
 
 <div class="hidden">
     <div id="call_order" class="popup">
@@ -73,80 +73,39 @@
     </div>
     <div id="conf" class="popup">
         <p class="popup_title">Политика конфиденциальности</p>
-        <div class="conf_wrap">
-            <?php the_field('conf_content', 'options'); ?>
-        </div>
+        <div class="conf_wrap"><?php the_field('conf_content', 'options'); ?></div>
     </div>
 </div>
 
-<?php wp_footer(); ?>
-
-<!-- Ленивая загрузка сторонних счётчиков (после взаимодействия/idle) -->
+<!-- ваши счетчики/пиксели (они уже async/defer) -->
+<!-- Begin LeadBack code -->
 <script>
-(function(w,d){
-  var fired=false;
-  function run3p(){
-    if(fired) return; fired=true;
-
-    // LeadBack
-    (function(){
-      var _emv = w._emv = w._emv || [];
-      _emv['campaign'] = '4251c47fdf0b437fa77b6685';
-      var s=d.createElement('script');
-      s.async=true;
-      s.src=(location.protocol==='https:'?'https://':'http://')+'leadback.ru/js/leadback.js';
-      d.head.appendChild(s);
-    })();
-
-    // VK pixel
-    (function(){
-      var vk=d.createElement('script');
-      vk.async=true; vk.src='https://vk.com/js/api/openapi.js?162';
-      vk.onload=function(){ try{ VK.Retargeting.Init('VK-RTRG-434995-hfA2Q'); VK.Retargeting.Hit(); }catch(e){} };
-      d.head.appendChild(vk);
-    })();
-
-    // Mail.ru
-    (function(){
-      var ts=d.createElement('script');
-      ts.async=true; ts.id='topmailru-code';
-      ts.src=(location.protocol==='https:'?'https:':'http:')+'//top-fwz1.mail.ru/js/code.js';
-      d.body.appendChild(ts);
-    })();
-  }
-
-  function arm(){
-    var once=function(){
-      run3p();
-      w.removeEventListener('scroll',once,{passive:true});
-      w.removeEventListener('touchstart',once,{passive:true});
-      w.removeEventListener('mousemove',once,{passive:true});
-      w.removeEventListener('keydown',once);
-    };
-    w.addEventListener('scroll',once,{passive:true});
-    w.addEventListener('touchstart',once,{passive:true});
-    w.addEventListener('mousemove',once,{passive:true});
-    w.addEventListener('keydown',once);
-    w.addEventListener('load',function(){
-      if('requestIdleCallback' in w){ requestIdleCallback(run3p,{timeout:5000}); }
-      setTimeout(run3p,3500); // страховка
-    });
-  }
-  arm();
-})(window, document);
+  var _emv=_emv||[]; _emv['campaign']='4251c47fdf0b437fa77b6685';
+  (function(){var em=document.createElement('script'); em.type='text/javascript'; em.async=true;
+  em.src=(document.location.protocol==='https:'?'https://':'http://')+'leadback.ru/js/leadback.js';
+  var s=document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(em, s);})();
 </script>
+<!-- End LeadBack code -->
 
-<!-- noscript-пиксели оставляем -->
-<noscript><img src="https://vk.com/rtrg?p=VK-RTRG-434995-hfA2Q" style="position:fixed;left:-999px;" alt=""/></noscript>
-<noscript><div><img src="//top-fwz1.mail.ru/counter?id=2944089;js=na" style="border:0;position:absolute;left:-9999px;" alt=""/></div></noscript>
+<!-- VK pixel -->
+<script>
+!function(){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://vk.com/js/api/openapi.js?162";
+t.onload=function(){VK.Retargeting.Init("VK-RTRG-434995-hfA2Q"),VK.Retargeting.Hit()};document.head.appendChild(t)}();
+</script>
+<noscript><img src="https://vk.com/rtrg?p=VK-RTRG-434995-hfA2Q" style="position:fixed;left:-999px;" alt=""></noscript>
 
-<!-- Скрипты темы (после wp_footer и с defer) -->
-<script src="<?php echo get_template_directory_uri(); ?>/js/owl.carousel/owl.carousel.min.js" defer></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/masked.input.js" defer></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/main.js" defer></script>
+<!-- Rating@Mail.ru counter -->
+<script>
+var _tmr=window._tmr||(window._tmr=[]);_tmr.push({id:"2944089",type:"pageView",start:(new Date()).getTime()});
+(function(d,w,id){if(d.getElementById(id))return;var ts=d.createElement("script");ts.type="text/javascript";ts.async=true;ts.id=id;
+ts.src=(d.location.protocol=="https:"?"https:":"http:")+"//top-fwz1.mail.ru/js/code.js";
+var f=function(){var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(ts,s);};
+if(w.opera=="[object Opera]"){d.addEventListener("DOMContentLoaded",f,false);}else{f();}})(document,window,"topmailru-code");
+</script>
+<noscript><div><img src="//top-fwz1.mail.ru/counter?id=2944089;js=na" style="border:0;position:absolute;left:-9999px;" alt=""></div></noscript>
 
+<?php wp_footer(); ?>
 <?php the_field('yandex_metrika', 'options'); ?>
 <?php the_field('google_analitics', 'options'); ?>
-
 </body>
 </html>
