617b372efbaa3af881a5e739c32ea5688ea10241
1krylova-stack <anastasia1991krylova@gmail.com>
2025-09-12 16:00:10 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ remove_action('wp_print_styles', 'print_emoji_styles');
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
-remove_action( 'wp_head', 'rsd_link' );
+remove_action( 'wp_head', 'rsd_link' ); 
 remove_action( 'wp_head', 'wlwmanifest_link' );
 remove_action( 'wp_head', 'feed_links_extra', 3 );
 
